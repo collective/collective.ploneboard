04084726fbb8c66bf199c0558e7b40a1026cae6b
Ammy2 <aman2arora2@gmail.com>
2014-05-22 23:46:45 +0530

    # As an admin I can add categories to the messageboard

--- src/collective/ploneboard/interfaces.py
+++ src/collective/ploneboard/interfaces.py
@@ -19,20 +19,27 @@ def possibleCategories(context):
             str(category_i)))
     return SimpleVocabulary(terms)
 
+
 class IMessageboard(form.Schema):
     category = schema.Text(
-            title=_(u"Categories for tagging Topics"),
-            description=_(u"Seperate by writing in different lines"),
-            required=True,
-            )
+        title=_(u"Categories for tagging Topics"),
+        description=_(u"Seperate by writing in different lines"),
+        required=True,
+        )
+
 
 class ITopic(form.Schema):
     category = schema.List(
-            title=_(u"Category"),
-            description=_(u"Choose to tag your Topic"),
-            required=True,
-            value_type= schema.Choice(title=_(u"Ssup"),source=possibleCategories,required=False)
+        title=_(u"Category"),
+        description=_(u"Choose to tag your Topic"),
+        required=True,
+        value_type=schema.Choice(
+            title=_(u"Ssup"),
+            source=possibleCategories,
+            required=False
             )
+        )
+
 
 class IConversation(form.Schema):
     """
