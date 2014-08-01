1451a576d407c007efd609900c08b152c9110e4c
Ammy2 <aman2arora2@gmail.com>
2014-08-02 02:34:32 +0530

    Form ready for merging conversations

--- src/collective/ploneboard/browser/conversation.py
+++ src/collective/ploneboard/browser/conversation.py
@@ -10,6 +10,16 @@ class ConversationView(BrowserView):
 
     def __call__(self):
         """"""
+        form_var = self.request.form
+        if 'conv_selected_to_merge' in form_var:
+            if 'merge_with_conv' in form_var:
+                if 'form.button.MergeConversationDone' in form_var:
+                    self.merge_them(
+                        form_var["conv_selected_to_merge"],
+                        form_var["merge_with_conv"]
+                        )
+                    return self.request.response.\
+                        redirect(self.context.absolute_url())
         return self.template()
 
     def givemergelink(self):
@@ -27,3 +37,7 @@ class ConversationView(BrowserView):
             self.context
             )
         return ans
+
+    def merge_them(self, merge_into=None, merge_from=None):
+        """"""
+        print "Hey, I'm a function which merges two conversations"
