bd86eefea4aa2d643a14579b4d687e368fb0859a
Timo Stollenwerk <stollenwerk@kitconcept.com>
2018-11-28 11:43:57 +0100

    Add dev status and license to pypi classifiers.

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,7 +4,8 @@ Changelog
 1.0.0a2 (unreleased)
 --------------------
 
-- Nothing changed yet.
+- Add license and dev status to pypi classifiers.
+  [timo]
 
 
 1.0.0a1 (2018-11-28)
--- setup.py
+++ setup.py
@@ -21,10 +21,12 @@ setup(name='collective.ploneboard',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+          "Development Status :: 3 - Alpha",
           "Environment :: Web Environment",
           "Framework :: Plone",
           "Framework :: Plone :: 5.0",
           "Framework :: Plone :: 5.1",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
           "Operating System :: OS Independent",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2.7",
